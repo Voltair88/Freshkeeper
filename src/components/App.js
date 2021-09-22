9761c8502705a19897a73428f3b3123d43912126
Voltair88 <70101120+Voltair88@users.noreply.github.com>
2021-09-22 15:55:38 +0200

    firebase auth part 1

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import Signup from "./Signup";
+import Dashboard from "./Dashboard"
+import Login from "./Login";
+import PrivateRoute from "./PrivateRoute";
+import React from "react";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../context/AuthContext";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,63 @@
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from "../context/AuthContext";
+import { Link, useHistory } from "react-router-dom";
+
+
+export default function Login() {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const { login } = useAuth();
+    const [error, setError] = useState("");   
+    const [loading, setLoading] = useState(false);
+    const history = useHistory();
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        try {
+            setError("")
+            setLoading(true)
+        await login(emailRef.current.value, passwordRef.current.value)
+        history.push("/")
+        } catch {
+            setError("Failed to log in")
+        }
+        setLoading(false);
+    }
+  return (
+    <div>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Login</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                ref={emailRef}
+                required
+              />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                ref={passwordRef}
+                required
+              />
+            </Form.Group>
+            <Button className="w-100 text-center mt-2" type="submit" disabled={loading}>
+              Login
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+      Need an account?<Link to="/signup">Sign up</Link>
+      </div>
+    </div>
+  );
+}
