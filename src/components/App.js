139ca24506afc53bf93c2c92dc7f5f849c010de7
Voltair88 <mattias.gulec@student.kyh.se>
2021-09-28 08:11:19 +0200

    added additem tab

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import PrivateRoute from "./Auth/PrivateRoute";
 import ForgotPassword from "./Auth/ForgotPassword";
 import UpdateProfile from "./Auth/UpdateProfile";
 import CenteredContainer from "./CenteredContainer";
+import AddItem from "./AddItem";
 import Header from "./Head";
 import BottomNavbar from "./Navbar";
 import React from "react";
@@ -14,18 +15,18 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <CenteredContainer>
-          <Header />
-          <BottomNavbar />
-
         <Router>
           <AuthProvider>
+          <Header />
             <Switch>
               <PrivateRoute exact path="/" component={Profile} />
               <PrivateRoute path="/update-profile" component={UpdateProfile} />
               <Route path="/signup" component={Signup} />
               <Route path="/login" component={Login} />
               <Route path="/forgot-password" component={ForgotPassword} />
+              <Route path="/additem" component={AddItem} />
             </Switch>
+            <BottomNavbar />
           </AuthProvider>
         </Router>
     </CenteredContainer>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Bnav } from "../styles";
 import { Icon } from "@iconify/react";
-
+import { NavLink } from "react-router-dom";
 export default function BottomNavbar() {
   return (
     <Bnav>
-      <div className="nav-button">
+        <div className="nav-button">
+      <NavLink to="/additem" activeClassName="Active">
         <Icon icon="ic:baseline-playlist-add" width="55" height="34" />
         <p>Add items</p>
+      </NavLink>
       </div>
       <div className="nav-button">
         <Icon icon="bx:bxs-fridge" width="40" height="34" />
