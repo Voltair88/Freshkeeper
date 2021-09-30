f871a5c60a8670e1731ab6a614ff0c6c40da97bf
Voltair88 <70101120+Voltair88@users.noreply.github.com>
2021-09-30 11:36:14 +0200

    additem day 1

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,9 +1,94 @@
-import React from 'react'
+import React, { useState } from "react";
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
+import { AI } from "../styles";
+import { Icon } from '@iconify/react';
 
 export default function AddItem() {
-    return (
-        <div>
-            add item
-        </div>
-    )
+  const [text, setText] = useState("");
+  const [day, setDay] = useState(new Date());
+
+  return (
+    <AI>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <form>
+          <div className="text">
+            <h3>Add Item</h3>
+            <p className="subhead ">
+              choose your product, quantity, storage and expiration date.{" "}
+            </p>
+          </div>
+          <hr />
+          <div className="item-name">
+            <p className="numbers">1</p>
+            <h3>Add product name</h3>
+            <input
+              className="name-input"
+              type="text"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              placeholder="Add product name"
+            />
+          </div>
+          <hr />
+          <div className="item-quantity">
+            <p className="numbers">2</p>
+            <h3>Choose quantity</h3>
+            <input className="number-input" type="number" />
+          </div>
+          <hr />
+          <p className="numbers">3</p>
+          <h3>Choose storage</h3>
+          <div className="storages">
+            <div className="storage-options active">
+            <Icon icon="bx:bx-fridge"  width="31" height="31"  />
+              <p>fridge</p>
+            </div>
+            <div className="storage-options">
+            <Icon icon="ph:thermometer-cold" width="31" height="31" />
+              <p>freezer</p>
+            </div>
+            <div className="storage-options">
+            <Icon icon="carbon:wheat" width="31" height="31" />
+              <p>Pantry</p>
+            </div>
+            <div className="storage-options">
+              <p>Custom storage</p>
+            </div>
+          </div>
+          <hr />
+          <div className="pick-day">
+            <p className="numbers">4</p>
+            <p>Choose Expiration Date</p>
+            <DatePicker value={day} onChange={(day) => setDay(day)} />
+          </div>
+          <hr />
+          <div className="remind">
+            <p>Remind me!</p>
+          </div>
+          <div className="summary">
+            <div className="summary-item">
+              <div className="summary-titel">product</div>
+              <p className="summary-subtitel">{text}</p>
+            </div>
+            <div className="summary-item">
+              <div className="summary-titel">storage</div>
+              <p className="summary-subtitel">fridge</p>
+            </div>
+            <div className="summary-item">
+              <div className="summary-titel">date</div>
+              <p className="summary-subtitel">7 days</p>
+            </div>
+            <div className="summary-item">
+              <div className="summary-titel">quantity</div>
+              <p className="summary-subtitel">quantity</p>
+            </div>
+          </div>
+          <div className="confirm">
+            <button>Confirm</button>
+          </div>
+        </form>
+      </MuiPickersUtilsProvider>
+    </AI>
+  );
 }
--- src/styles.js
+++ src/styles.js
@@ -1,93 +1,180 @@
 import styled from "styled-components";
+import AddItem from "./components/AddItem";
 
 /* Top Nav */
-export const Header = styled.header `
-@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+export const Header = styled.header`
+  @import url("https://fonts.googleapis.com/css2?family=Pacifico&display=swap");
 
-/* Auto Layout */
-display: flex;
-flex-direction: row;
-justify-content: center;
-align-items: center;
-padding: 14px 16px 14px 18px;
-position: absolute;
-height: 65px;
-left: 0px;
-right: 0px;
-top: 0px;
-background: #0E4462;
+  /* Auto Layout */
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 14px 16px 14px 18px;
+  position: absolute;
+  height: 65px;
+  left: 0px;
+  right: 0px;
+  top: 0px;
+  background: #0e4462;
 
-h1 {
-position: static;
-height: 31px;
-top: 17px;
-font-family: Pacifico;
-font-size: 20px;
-line-height: 20px;
-/* or 100% */
-display: flex;
-align-items: center;
-text-align: center;
-color: #49BEFF;
+  h1 {
+    position: static;
+    height: 31px;
+    top: 17px;
+    font-family: Pacifico;
+    font-size: 20px;
+    line-height: 20px;
+    /* or 100% */
+    display: flex;
+    align-items: center;
+    text-align: center;
+    color: #49beff;
 
-/* Inside Auto Layout */
+    /* Inside Auto Layout */
 
-flex: none;
-order: 0;
-flex-grow: 0;
-margin: 0px 10px;
-}
-
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+    margin: 0px 10px;
+  }
 `;
 
 /* Bottom Nav */
 
-export const Bnav = styled.nav `
-display: flex;
-flex-direction: row;
-justify-content: center;
-align-items: space-between;
-position: absolute;
-height: 65px;
-left: 0px;
-right: 0px;
-bottom: 0px;
-background: #033B5B;
-color: #49BEFF;
-padding: 8px 16px 14px 18px;
+export const Bnav = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: space-between;
+  position: absolute;
+  height: 65px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  background: #033b5b;
+  color: #49beff;
+  padding: 8px 16px 14px 18px;
 
-
-.nav-button {
+  .nav-button {
     font-size: 12px;
     margin: 0px auto;
     bottom: 0px;
-}
+  }
 
-a {
+  a {
     text-decoration: none;
-}
+  }
 
-.Active {
+  .Active {
     color: white;
-}
+  }
 `;
 
-export const Acc = styled.div `
-display: flex;
-flex-direction: column;
-justify-content: top;
-align-items: space-between;
+export const Acc = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: top;
+  align-items: space-between;
 
-
-
-.account-tab {
+  .account-tab {
     padding: 6px;
     margin: 2px 0px;
     left: 0px;
     right: 0px;
     background: aliceblue;
-}
-p {
+  }
+  p {
     margin-bottom: 0;
-}
+  }
+`;
+
+export const AI = styled.div`
+  form {
+    @import url("https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap");
+  }
+  .subhead {
+    font-family: Inter, sans-serif;
+    font-style: italic;
+    font-weight: 300;
+    font-size: 12px;
+    line-height: 30px;
+    margin: auto 8%;
+    min-width: 100%;
+  }
+  h3 {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: auto;
+    min-width: 33%;
+    font-family: Inter, sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 26px;
+    line-height: 30px;
+    color: #000000;
+  }
+  .numbers {
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 900;
+    font-size: 12px;
+    position: absolute;
+    line-height: 50%;
+    margin: 0px;
+    background-color: #49beff;
+    padding: 10px;
+    border-radius: 100vw;
+  }
+  input {
+    margin: auto;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    width: 276px;
+    height: 41px;
+    left: 66px;
+    top: 200px;
+    background: rgba(17, 169, 255, 0.12);
+    border: 1px solid #49beff;
+    box-sizing: border-box;
+    border-radius: 5px;
+  }
+  .number-input {
+    font-size: 36px;
+    text-align: center;
+    width: 75px;
+    height: 76px;
+    left: 169px;
+    top: 314px;
+    border-radius: 100vw;
+    background: #ffffff;
+    border: 2px solid #49beff;
+    box-sizing: border-box;
+  }
+  .storages {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: space-between;
+    height: 65px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+  }
+  .storage-options {
+    font-family: Inter, sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    width: 76px;
+    height: 76px;
+    
+    border-radius: 100vw;
+    border: 2px solid #49beff;
+    text-align: center;
+    letter-spacing: 0.015em;
+    text-transform: lowercase;
+  }
 `;
